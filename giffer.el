58f2a787211d46a050397707c1200166279de1fc
Simon Pitt <si_pitt@yahoo.com>
2025-10-22 16:19:37 +0100

    Refactor. Move utility functions to utility packages.

--- giffer.el
+++ giffer.el
@@ -1,61 +1,55 @@
 ;;; giffer.el --- Clip gifs from video files  -*- lexical-binding: t -*-
 
-(defun img/dired-sp-convert-to-gif-2 ()
- "?"
- (interactive)
- (let* ((directory (dired-current-directory))
-	(filename (dired-get-filename 'no-dir))
-	(basename (file-name-base filename))
-	(cmd-buffer (generate-new-buffer "*Convert to gif*")))
-   (display-buffer cmd-buffer '((display-buffer-pop-up-window)))
-   (select-window (get-buffer-window  cmd-buffer))
-   (txt/insert-sp-text-comment "# C-c C-c to execute\n# C-c C-k to cancel\n\n")
-   (txt/insert-sp-labelled-line
-    `(("Directory: " . ,directory)
-      ("Input file: " . ,filename)
-      ("Output file: " . ,(format "%s.gif" basename))
-      ("Start: " . "00:00:00")
-      ("End: " . "00:00:00")
-      ("Dither mode: " . "bayer")
-      ("FPS: " . "15")))
-   (sp/define-field
-    (sp/find-regex-bounds "bayer$")
-    'gif-dither-field
-    gif-dither-field-keymap)
-   (sp/define-field
-    (sp/find-regex-bounds "15$")
-    'gif-fps-field
-    gif-fps-field-keymap)
-   (goto-line 7)
-   (goto-char (line-end-position))
-   (local-set-key (kbd "C-c C-c") #'gif-compose-cmd-from-buffer-lines)
-   (local-set-key (kbd "C-c C-k") #'sp/kill-this-buffer)))
+(require 'sp-form-funcs)
+(require 'sp-utils)
 
-(defun sp/define-field (bounds field-sym keymap)
-  "Make region START to END a field with RET keymap."
-  (let ((start (car bounds))
-	(end   (cdr bounds)))
-    (put-text-property start end field-sym t)
-    (put-text-property start end 'keymap keymap)
-    (put-text-property start end 'read-only nil)
-    (put-text-property start end 'face 'highlight)))
-
-(defun sp/get-field-bounds (field-sym)
-  "Return (START . END) of the field under point, or nil."
+;; setup initial window
+(defun img/dired-sp-convert-to-gif-windowed ()
+  "Open a buffer to set arguments to be passed to ffmpeg to convert
+   part of a video file to a high-quality gif."
   (interactive)
-  (let ((start (previous-single-property-change (point) field-sym))
-        (end (next-single-property-change (point) field-sym)))
-    (when (get-text-property (point) field-sym)
-      (cons (or start (point-min))
-            (or end (point-max))))))
+  (let* ((directory (dired-current-directory))
+	 (filename (dired-get-filename 'no-dir))
+	 (basename (file-name-base filename))
+	 (cmd-buffer (generate-new-buffer "*Convert to gif*"))
+	 (lines `(("Directory: " . ,directory)
+		  ("Input file: " . ,filename)
+		  ("Output file: " . ,(format "%s.gif" basename))
+		  ("Start: " . "00:00:00")
+		  ("End: " . "00:00:00")
+		  ("Dither mode: " . "bayer")
+		  ("FPS: " . "15"))))
 
-(defun sp/find-regex-bounds (regex &optional start end)
-  "Search for REGEX in the current buffer between START and END.
-Return a cons cell (START . END) of the first match, or nil if not found."
-  (save-excursion
-    (goto-char (or start (point-min)))
-    (when (re-search-forward regex end t)
-      (cons (match-beginning 0) (match-end 0)))))
+    ;; Create buffer and write text
+    (display-buffer cmd-buffer '((display-buffer-below-selected)))
+    (select-window (get-buffer-window  cmd-buffer))
+    (txt/insert-sp-text-comment "# C-c C-c to execute\n# C-c C-k to cancel\n\n")
+    (txt/insert-sp-labelled-line lines "%-14s")
+
+    ;; define fields
+    ;; (sp/define-field (sp/find-regex-bounds "bayer$") 'gif-dither-field gif-dither-field-keymap)
+    ;; (sp/define-field (sp/find-regex-bounds "15$") 'gif-fps-field gif-fps-field-keymap)
+    (dolist (pair `(("bayer$" . (gif-dither-field . ,gif-dither-field-keymap))
+		    ("15$" . (gif-fps-field . ,gif-fps-field-keymap))))
+      (let* ((rgx (car pair))
+	     (args (cdr pair))
+	     (field-sym (car args))
+	     (keymap (cdr args)))
+	(message "%S %S %S %S" rgx args field-sym keymap)
+	(sp/define-field
+	 (sp/find-regex-bounds rgx) field-sym keymap)))
+
+    ;; Set line headings as read only
+    (dolist (line-pair lines)
+      (let* ((rgx (car line-pair)))
+	(sp/set-rgx-range-readonly rgx)))
+
+    ;; finish setup
+    (goto-line 6)
+    (goto-char (- (line-end-position) 4))
+    (undo-tree-mode 1)
+    (local-set-key (kbd "C-c C-c") #'gif-compose-cmd-from-buffer-lines)
+    (local-set-key (kbd "C-c C-k") #'kill-buffer-and-window)))
 
 (defun gif-dither-field-complete ()
   "Prompt for a value with completing-read and replace field contents."
@@ -96,44 +90,87 @@ Return a cons cell (START . END) of the first match, or nil if not found."
     map)
   "Keymap active when cursor is in a gif-fps-field.")
 
-(defun txt/insert-sp-text-comment (txt)
-  "Insert text propertized with the `font-lock-comment-face'"
-  (insert (propertize txt 'face 'font-lock-comment-face)))
-
-
-(defun txt/insert-sp-labelled-line (elems)
-  (dolist (pair elems)
-    (let ((key (car pair))
-	  (value (cdr pair)))
-      (insert
-       (propertize (format "%-14s" key) 'face 'font-lock-keyword-face)
-       (propertize value 'face 'font-lock-string-face)
-       "\n"))))
-
-
-(defun sp/collect-arg-from-buffer (prefix)
-  "Return the captured part of the first line starting with PREFIX, or nil."
-  (save-excursion
-    (goto-char (point-min))
-    (when (re-search-forward (concat "^" (regexp-quote prefix) "\\s-*\\(.*\\)$") nil t)
-      (match-string-no-properties 1))))
-
 (defun gif-compose-cmd-from-buffer-lines ()
   (interactive)
-  (let ((directory (sp/collect-arg-from-buffer "Directory: "))
+  (let* ((directory (sp/collect-arg-from-buffer "Directory: "))
 	(input-file (sp/collect-arg-from-buffer "Input file: "))
 	(output-file (sp/collect-arg-from-buffer "Output file: "))
 	(start-time (sp/collect-arg-from-buffer "Start: "))
 	(end-time (sp/collect-arg-from-buffer "End: "))
 	(dither-mode (sp/collect-arg-from-buffer "Dither mode: "))
-	(fps (sp/collect-arg-from-buffer "FPS: ")))
-    (message "%s %s %s %s %s %s %s" directory input-file output-file start-time end-time dither-mode fps)))
+	(fps (sp/collect-arg-from-buffer "FPS: "))
+	(palette-file "/tmp/palette%02d.png")
+	(cmd (concat
+	      "ffmpeg -ss " start-time
+	      " -to " end-time
+	      " -i " input-file
+	      " -vf \"fps=" fps
+	      ",scale=480:-1:flags=lanczos,palettegen=stats_mode=diff\""
+	      " -y " palette-file "; "
+	      "ffmpeg -ss " start-time
+	      " -to " end-time
+	      " -i " input-file
+	      " -i " palette-file
+	      " -lavfi \"fps=" fps
+	      ",scale=480:-1:flags=lanczos [x]; [x][1:v] paletteuse=dither=" dither-mode
+	      "\" -loop 0 -y " output-file "; "
+	      "rm -vf /tmp/palette*.png")))
+    (message "%s %s %s %s %s %s %s" directory input-file output-file start-time end-time dither-mode fps)
+    (start-process "convert-to-gif" "*ffmpeg conversion*" "bash" "-c" cmd)
+    (kill-buffer-and-window)))
 
 
-(defun sp/kill-this-buffer ()
-  (interactive)
-  (kill-buffer (current-buffer)))
+;; (defun sp/define-field (bounds field-sym keymap)
+;;   "Make region START to END a field with RET keymap."
+;;   (let ((start (car bounds))
+;; 	(end   (cdr bounds)))
+;;     (put-text-property start end field-sym t)
+;;     (put-text-property start end 'keymap keymap)
+;;     (put-text-property start end 'read-only nil)
+;;     (put-text-property start end 'face 'highlight)))
 
+;; (defun sp/set-rgx-range-readonly (rgx)
+;;     (let* ((bounds (sp/find-regex-bounds rgx))
+;; 	   (start (car bounds))
+;; 	   (end (cdr bounds)))
+;;       (put-text-property start end 'read-only t)))
+
+;; (defun sp/get-field-bounds (field-sym)
+;;   "Return (START . END) of the field under point, or nil."
+;;   (interactive)
+;;   (let ((start (previous-single-property-change (point) field-sym))
+;;         (end (next-single-property-change (point) field-sym)))
+;;     (when (get-text-property (point) field-sym)
+;;       (cons (or start (point-min))
+;;             (or end (point-max))))))
+
+;; (defun sp/find-regex-bounds (regex &optional start end)
+;;   "Search for REGEX in the current buffer between START and END.
+;; Return a cons cell (START . END) of the first match, or nil if not found."
+;;   (save-excursion
+;;     (goto-char (or start (point-min)))
+;;     (when (re-search-forward regex end t)
+;;       (cons (match-beginning 0) (match-end 0)))))
+
+;; (defun txt/insert-sp-text-comment (txt)
+;;   "Insert text propertized with the `font-lock-comment-face'"
+;;   (insert (propertize txt 'face 'font-lock-comment-face)))
+
+;; (defun txt/insert-sp-labelled-line (elems fmt-str)
+;;   (dolist (pair elems)
+;;     (let ((key (car pair))
+;; 	  (value (cdr pair)))
+;;       (insert
+;;        (propertize (format fmt-str key) 'face 'font-lock-keyword-face)
+;;        (propertize value 'face 'font-lock-string-face)
+;;        "\n"))))
+
+;; (defun sp/collect-arg-from-buffer (prefix)
+;;   "Return the captured part of the first line starting with PREFIX, or nil."
+;;   (save-excursion
+;;     (goto-char (point-min))
+;;     (when (re-search-forward (concat "^" (regexp-quote prefix) "\\s-*\\(.*\\)$") nil t)
+;;       (match-string-no-properties 1))))
 
 (provide 'giffer)
 
